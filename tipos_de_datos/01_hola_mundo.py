511654938ca70e30ed0c61cbaa7404606aaddb4d
Lopez Victor Fernando <victorfernandolopez1988@gmail.com>
2024-05-26 21:41:33 -0300

    01- imprimir hola mundo

---
+++ tipos_de_datos/01_hola_mundo.py
@@ -0,0 +1,10 @@
+"""
+Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
+"""
+print("¡Hola Mundo!")
+
+"""
+Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por pantalla el contenido de la variable.
+"""
+cadena = "¡Hola Mundo!"
+print(cadena)
