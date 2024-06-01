1eba519c74e4426865c46a5c783db9f7227a6c9c
Lopez Victor Fernando <victorfernandolopez1988@gmail.com>
2024-05-31 21:21:31 -0300

    mayor o menor

---
+++ condicionales/01_ejercicio1.py
@@ -0,0 +1,8 @@
+"""
+Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+"""
+edad = int(input("ingrese su edad: "))
+if edad >= 18:
+    print("es mayor")
+else:
+    print("es menor")
