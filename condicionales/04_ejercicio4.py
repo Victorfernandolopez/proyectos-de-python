ecc4c9c55c79730593368944c0ad3ddc27616ed7
Lopez Victor Fernando <victorfernandolopez1988@gmail.com>
2024-05-31 22:05:14 -0300

    par o inpar

---
+++ condicionales/04_ejercicio4.py
@@ -0,0 +1,8 @@
+"""
+Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+"""
+numero = int(input("ingrese un numero entero: "))
+if numero%2==0:
+    print("es par")
+else:
+    print("es inpar")
