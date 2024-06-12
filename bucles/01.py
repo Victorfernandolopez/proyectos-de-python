b3948a28c7d31730770ee50586f0c4ab3ac219d0
Lopez Victor Fernando <victorfernandolopez1988@gmail.com>
2024-06-11 21:01:30 -0300

    repetir una palabra 10 veces

---
+++ bucles/01.py
@@ -0,0 +1,15 @@
+"""
+Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
+"""
+#pedimos el dato
+palabra = input("ingrese una palabra: ")
+
+#ciclo for
+for i in range(10):
+    print(palabra)
+
+#ciclo while
+i=0
+while i <11:
+    print(palabra)
+    i+=1
